318e48c0c5ec9254b5370565d41efde35c746dbc
landyliga <ligalandry@gmail.com>
2025-02-25 19:32:59 +0100

    maj du module

--- src/Plugin/Block/PopupBlock.php
+++ src/Plugin/Block/PopupBlock.php
@@ -6,7 +6,9 @@ namespace Drupal\hbk_popin\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
-
+use Drupal\layoutgenentitystyles\Services\LayoutgenentitystylesServices;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 /**
  * Provides a popup block.
  *
@@ -16,7 +18,26 @@ use Drupal\Core\Form\FormStateInterface;
  *   category = @Translation("Custom"),
  * )
  */
-final class PopupBlock extends BlockBase {
+final class PopupBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+   /**
+   *
+   * @var LayoutgenentitystylesServices
+   */
+  protected $LayoutgenentitystylesServices;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LayoutgenentitystylesServices $LayoutgenentitystylesServices) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->LayoutgenentitystylesServices = $LayoutgenentitystylesServices;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('layoutgenentitystyles.add.style.theme'));
+  }
 
   /**
    * {@inheritdoc}
@@ -24,6 +45,9 @@ final class PopupBlock extends BlockBase {
   public function defaultConfiguration(): array {
     return [
       'message' => '',  // Message par défaut
+      'class_container' => 'right button',
+      'block_load_style_scss_js' => 'hbk_popin/custom-buton',
+      'reseller_page' => FALSE,
     ];
   }
 
@@ -39,6 +63,20 @@ final class PopupBlock extends BlockBase {
       '#description' => $this->t('Saisissez le message que vous souhaitez afficher dans le popup.'),
     ];
 
+    // Ajoute un champ texte pour la classe du conteneur
+    $form['class_container'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Class container'),
+      '#default_value' => $this->configuration['class_container'],
+    ];
+
+    // Ajoute un champ pour la page de revendeur
+    $form['reseller_page'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Reseller Page'),
+      '#default_value' => $this->configuration['reseller_page'],
+    ];
+
     return $form;
   }
 
@@ -48,6 +86,11 @@ final class PopupBlock extends BlockBase {
   public function blockSubmit($form, FormStateInterface $form_state): void {
     // Sauvegarde la valeur du champ 'message' dans la configuration
     $this->configuration['message'] = $form_state->getValue('message');
+    // Sauvegarde la valeur de 'class_container' et 'reseller_page'
+    $this->configuration['class_container'] = $form_state->getValue('class_container');
+    $this->configuration['reseller_page'] = $form_state->getValue('reseller_page');
+    $library = $this->configuration['block_load_style_scss_js'];
+    $this->LayoutgenentitystylesServices->addStyleFromModule($library, 'hbk_you_custom_popup', 'default');
   }
 
   /**
---
+++ wbu-atomique-theme/src/js/custom-buton.js
@@ -0,0 +1,2 @@
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/restoRed/redModel/sectionbouton.scss";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/restoRed/js/teaserbutonwhat.js";
